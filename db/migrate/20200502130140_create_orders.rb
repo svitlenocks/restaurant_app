122a9cfbdf76939feea450b3fbd1917316db08ce
Vlad Kamysh <svitlenocks23@gmail.com>
2020-05-02 16:06:03 +0300

    4: Add Order and OrderItem models

---
+++ db/migrate/20200502130140_create_orders.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.decimal :subtotal
+      t.decimal :total
+
+      t.timestamps
+    end
+  end
+end
