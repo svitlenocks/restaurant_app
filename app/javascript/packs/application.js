8fdd7d928ab27aff53184932684a86991644a61b
Vlad Kamysh <svitlenocks23@gmail.com>
2020-05-02 13:10:34 +0300

    0: Initial commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,12 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import "bootstrap";
+import "./src/application";
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
